a9a53b07e138c2cc2546eaf36c8b69ff7b06836a
Emanuel Coen <emanuelcoen@gmail.com>
2020-01-20 10:57:35 +1100

    second commit

---
+++ src/components/gif_list.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Gif from './gif.jsx';
+
+class GifList extends Component {
+  constructor(props) {
+    super(props) ;
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "xT9IgDEI1iZyb2wqo8"
+    }
+  }
+  renderList = () => {
+    return this.props.gifs.map(gif => {
+        return <Gif id={gif.id} key={gif.id} />
+      })
+  }
+
+  render() {
+    return (
+      <div className="gif-list">
+        {this.renderList()}
+      </div>
+    );
+  }
+}
+
+export default GifList;
+
---
+++ src/components/search_bar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+    <input type ="text" className="form-control form-search" onUpdate={this.handleUpdate}/>
+    );
+  }
+
+}
+
+export default SearchBar;
